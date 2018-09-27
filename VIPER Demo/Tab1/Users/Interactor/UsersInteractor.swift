3feb2c221c651c21a6ae57e54eca7971b7f5d1eb
Tarunp123 <tarunp123@gmail.com>
2018-09-27 08:43:53 +0530

    UsersInteractor.swift can now parse the JSON response and create Users and Messages Object.

--- VIPER Demo/Tab1/Users/Interactor/UsersInteractor.swift
+++ VIPER Demo/Tab1/Users/Interactor/UsersInteractor.swift
@@ -19,9 +19,45 @@ class UsersInteractor: UsersInteractorInputProtocol {
         
         let httpRequestor = HTTPRequestor(urlString: self.postsURLString)
         httpRequestor.makeRequest { (responseData, error) in
+            
+            //Error Checking
+            guard error == nil else{
+                print(error.debugDescription)
+                return
+            }
+            
             if let data = responseData{
-                if let posts = try? JSONDecoder().decode([PostDTO].self, from: data){
-                    print("POSTS = \(posts.count)")
+                do{
+ 
+                    let posts = try JSONDecoder().decode([PostDTO].self, from: data)
+                    //parsing done
+                    
+                    //create Users and their Messages
+                    var usersMap = Dictionary<Int, User>()
+                    
+                    for index in 0..<posts.count{
+                        
+                        if usersMap[posts[index].userId] == nil{
+                            //User doesn't exists in map
+                            //Add new User
+                            usersMap[posts[index].userId] = User(id: posts[index].userId)
+                        }
+                        //append new message
+                        usersMap[posts[index].userId]!.messages.append(Message(title: posts[index].title, body: posts[index].body))
+                    }
+                    
+                    //Sort Users based on user id
+                    let sortedRecords = usersMap.sorted(by: { (record1, record2) -> Bool in
+                        return record1.key < record2.key
+                    })
+                    
+                    for record in sortedRecords{
+                        print("\(record.value.id) = \(record.value.messages.count)")
+                    }
+                    
+                }catch{
+                    //error
+                    print(error)
                 }
             }
         }
